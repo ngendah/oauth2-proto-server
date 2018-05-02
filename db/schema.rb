0e72b44ce171259a9b004a1de3084a7b140d1cff
ngenda <ngendahk@gmail.com>
2018-05-02 18:38:26 +0300

    Update migration schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,39 +14,39 @@ ActiveRecord::Schema.define(version: 2018_04_26_082453) do
 
   create_table "access_tokens", force: :cascade do |t|
     t.string "token", null: false
-    t.integer "client_id"
-    t.integer "user_id"
     t.datetime "expires", null: false
     t.integer "scopes"
     t.boolean "refresh", default: false
+    t.boolean "deleted", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["client_id"], name: "index_access_tokens_on_client_id"
     t.index ["token"], name: "index_access_tokens_on_token"
-    t.index ["user_id"], name: "index_access_tokens_on_user_id"
+  end
+
+  create_table "access_tokens_authorization_codes", id: false, force: :cascade do |t|
+    t.integer "access_token_id"
+    t.integer "authorization_code_id"
+    t.index ["access_token_id"], name: "idx_token_code_token_id"
+    t.index ["authorization_code_id"], name: "idx_token_code_auth_cod_id"
   end
 
   create_table "authorization_codes", force: :cascade do |t|
     t.string "code", null: false
     t.integer "client_id"
-    t.integer "user_id"
     t.string "redirect_url"
     t.datetime "expires", null: false
     t.integer "scopes"
-    t.integer "access_token_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["access_token_id"], name: "index_authorization_codes_on_access_token_id"
     t.index ["client_id"], name: "index_authorization_codes_on_client_id"
     t.index ["code"], name: "index_authorization_codes_on_code"
-    t.index ["user_id"], name: "index_authorization_codes_on_user_id"
   end
 
   create_table "clients", force: :cascade do |t|
     t.string "uid", null: false
     t.string "secret", null: false
-    t.string "redirect_uri"
-    t.string "grant_type"
+    t.string "redirect_url"
+    t.string "grant_types"
     t.integer "scope"
     t.integer "user_id"
     t.datetime "created_at", null: false
