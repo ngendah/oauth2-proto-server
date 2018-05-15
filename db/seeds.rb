a667d03d9de56f6d31666d4d8510c0df50cbe8ae
ngenda <ngendahk@gmail.com>
2018-05-15 11:10:49 +0300

    add development seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env.development?
+  user_uid = '9c965d6d-ec9d-45de-9708-13f3f62d7c4d'
+  client_uid = 'c2ce91a6-98b6-4d4b-99ad-eeb174c0b6d5'
+  user = User.create uid: user_uid, password: 'password'
+  Client.create uid: client_uid, secret: 'secret', redirect_url: 'https://mytest.co', users: [user]
+end
