907cbeb7b3ab69d560e746e3675398d8ae5f3b72
ngenda <ngendahk@gmail.com>
2018-12-29 19:58:27 +0300

    Update docs controller to handle docs menu

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    render json: YAML.load_file("#{Rails.root}/docs/openapi/api-doc.yml")
+    render json: YAML.load_file("#{Rails.root}/docs/openapi/#{params[:docs]}.yml")
+  rescue
+    render json: YAML.load_file("#{Rails.root}/docs/openapi/default.yml")
   end
 end
