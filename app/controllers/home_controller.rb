b67ba2d06a00c7a470f35ed19b641f0c0f950a04
ngenda <ngendahk@gmail.com>
2018-12-30 16:28:14 +0300

    Update doc routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    render json: YAML.load_file("#{Rails.root}/docs/openapi/#{params[:docs]}.yml")
+    render json: YAML.load_file("#{Rails.root}/docs/openapi/#{params[:doc]}.yml")
   rescue
     render json: YAML.load_file("#{Rails.root}/docs/openapi/default.yml")
   end
